4b8476be756e09ced45f484c379980899c2f4010
nayhub <antguajardo2021@udec.cl>
2023-11-01 23:18:39 -0300

    feat: creación de JFrame

---
+++ src/GUI/Main.java
@@ -0,0 +1,21 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main{
+
+    public static void main(String[] args){
+
+        JFrame frame = new JFrame();
+        frame.setTitle("EXPENDEDOR");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+        frame.setVisible(true);
+        frame.getContentPane().setBackground(Color.pink);
+
+    }
+
+
+}
