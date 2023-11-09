6cc14ccb5954e2c32f079e79b444eadbd36b3f61
Jhonweedmann <benjaminjr2004@gmail.com>
2023-11-09 17:49:09 -0300

    feat: Nuevo tipo de moneda con valor modificable

--- src/Tarea2/MonedaEspecial.java
+++ src/Tarea2/MonedaEspecial.java
@@ -1,2 +1,17 @@
-package Tarea2;public class MonedaEspecial {
+package Tarea2;
+
+public class MonedaEspecial extends Moneda {
+    private int Valor;
+
+    public MonedaEspecial(){ }
+    @Override
+    public int getValor() {
+        return Valor;
+    }
+    public void addValor (int a){
+        Valor += a;
+    }
+    public void reiniciarValor(){
+        Valor = 0;
+    }
 }
