644c8a38234e6769cdff87a5743a6821e6caca90
nayhub <antguajardo2021@udec.cl>
2023-11-10 19:57:36 -0300

    feat: javadoc de Super8Deposito.

--- src/GUI/Super8Deposito.java
+++ src/GUI/Super8Deposito.java
@@ -5,13 +5,25 @@ import Tarea2.Expendedor;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Subclase de VentanaDeposito que sirve como deposito para Super8.
+ */
 public class Super8Deposito extends VentanaDeposito {
     private Expendedor expendedor;
+
+    /**
+     * Constructor para la clase Super8Deposito.
+     *
+     * @param expendedor Una instancia de la clase Expendedor.
+     */
     public Super8Deposito(Expendedor expendedor) {
         super(expendedor);
         this.expendedor = expendedor;
     }
 
+    /**
+     * Agrega un elemento a la lista elementos y repinta el componente.
+     */
     @Override
     public void agregarElemento() {
         elementos.add("Super8");
