7b7ca142e308ff5f54affbd2bb9c470d468b9cf6
nayhub <antguajardo2021@udec.cl>
2023-11-10 19:57:26 -0300

    feat: javadoc de SpriteDeposito.

--- src/GUI/SpriteDeposito.java
+++ src/GUI/SpriteDeposito.java
@@ -5,13 +5,25 @@ import Tarea2.Expendedor;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Subclase de VentanaDeposito que sirve como deposito para Sprite
+ */
 public class SpriteDeposito extends VentanaDeposito {
     private Expendedor expendedor;
+
+    /**
+     * Constructor para la clase SpriteDeposito.
+     *
+     * @param expendedor Una instancia de la clase Expendedor.
+     */
     public SpriteDeposito(Expendedor expendedor) {
         super(expendedor);
         this.expendedor = expendedor;
     }
 
+    /**
+     * Agrega un elemento a la lista elementos y repinta el componente.
+     */
     @Override
     public void agregarElemento() {
         elementos.add("Sprite");
