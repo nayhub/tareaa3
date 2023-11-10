dd2b2f7249fefa50dcd3512022e2957c5b2a1738
nayhub <antguajardo2021@udec.cl>
2023-11-10 19:57:02 -0300

    feat: javadoc de FantaDeposito.

--- src/GUI/FantaDeposito.java
+++ src/GUI/FantaDeposito.java
@@ -5,13 +5,25 @@ import Tarea2.Expendedor;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * subclase de VentanaDeposito que sirve como deposito para Fanta.
+ */
 public class FantaDeposito extends VentanaDeposito {
     private Expendedor expendedor;
+
+    /**
+     * Constructor para la clase FantaDeposito.
+     *
+     * @param expendedor Una instancia de la clase Expendedor.
+     */
     public FantaDeposito(Expendedor expendedor) {
         super(expendedor);
         this.expendedor = expendedor;
     }
 
+    /**
+     * Agrega un elemento a la lista elementos y repinta el componente.
+     */
     @Override
     public void agregarElemento() {
         elementos.add("Fanta");
