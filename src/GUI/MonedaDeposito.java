11fb0c2b92cdde3d0072f7c24819fc65e657950e
nayhub <antguajardo2021@udec.cl>
2023-11-10 20:30:58 -0300

    feat: javadoc de MonedaDeposito.

--- src/GUI/MonedaDeposito.java
+++ src/GUI/MonedaDeposito.java
@@ -9,11 +9,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 
+/**
+ * Clase que extiende a JPanel y representa el deposito de monedas.
+ */
+
 public class MonedaDeposito extends JPanel {
     private PanelComprador panelComprador;
     protected ArrayList <String> depositoMonedas;
     private Moneda moneda;
     private MonedaEspecial monedaEspecial;
+
+    /**
+     * Constructor para la clase MonedaDeposito.
+     *
+     * @param panelComprador Una instancia de la clase panelComprador.
+     */
     public MonedaDeposito(PanelComprador panelComprador) {
         this.panelComprador = panelComprador;
         depositoMonedas = new ArrayList<>();
@@ -21,17 +31,32 @@ public class MonedaDeposito extends JPanel {
         //moneda100Deposito = new Moneda100Deposito(this);
 
     }
+
+    /**
+     * Obtiene el valor total de la moneda.
+     *
+     * @return Una instancia de la clase Moneda.
+     */
     public Moneda getMonedaTotal(){
         monedaEspecial.reiniciarValor();
         monedaEspecial.addValor(panelComprador.precioTotalmonedasArraylist());
         return monedaEspecial;
     }
+
+    /**
+     * Transforma la moneda a una cadena de caracteres.
+     *
+     * @param a Un entero que representa el índice de la moneda.
+     */
     //transformar de moneda a String
     public void getStringMonedaArraylist(int a){
         String moneda = panelComprador.monedasArraylist.get(a).getValor() + "";
         depositoMonedas.add(moneda);
     }
 
+    /**
+     * Borra las monedas del depósito y repinta el componente.
+     */
     public void borrarMonedas(){
         depositoMonedas.clear();
         //panelComprador.BorrarMonedasArrayList();
@@ -60,6 +85,9 @@ public class MonedaDeposito extends JPanel {
 
     }
 
+    /**
+     * Obtiene la lista de monedas del depósito.
+     */
     public ArrayList<String> getDepositoMonedas() {
         return depositoMonedas;
     }
