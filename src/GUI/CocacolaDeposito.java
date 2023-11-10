856c46d356b02c1d24c9393f1001c66502d66982
nayhub <antguajardo2021@udec.cl>
2023-11-10 19:56:48 -0300

    feat: javadoc de CocacolaDeposito.

--- src/GUI/CocacolaDeposito.java
+++ src/GUI/CocacolaDeposito.java
@@ -5,13 +5,25 @@ import Tarea2.Expendedor;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Subclase de VentanaDeposito que sirve como deposito para CocaCola.
+ */
 public class CocacolaDeposito extends VentanaDeposito {
     private Expendedor expendedor;
+
+    /**
+     * Constructor para la clase CocacolaDeposito.
+     *
+     * @param expendedor Una instancia de la clase Expendedor.
+     */
     public CocacolaDeposito(Expendedor expendedor) {
         super(expendedor);
         this.expendedor = expendedor;
     }
 
+    /**
+     * Agrega un elemento a la lista elementos y repinta el componente.
+     */
     @Override
     public void agregarElemento() {
         elementos.add("Cocacola");
